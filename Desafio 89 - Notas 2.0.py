7186fecdd0db823a506700593296ed0d269fc043
José Valdeir <43117843+Rumanns@users.noreply.github.com>
2022-04-29 21:22:21 -0300

    Delete Desafio 89 - Notas 2.0.py

--- Desafio 89 - Notas 2.0.py
+++
@@ -1,6 +0,0 @@
-"""
- Title: Dados escolares usando dicionário
- Objective: Buscar dados num dicionário e exibir na tela a partir da busca de um usuário.
- Author: José Valdeir
- Date: 29/04/2022
-"""
